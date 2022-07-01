af28a5b39921ed27f672a824dd1dec73ad718f71
Ethan Myos <myos0004@umn.edu>
2022-07-01 18:35:37 -0500

    finished build 1 of game

--- script.js
+++ script.js
@@ -1,128 +1,244 @@
-const gameBoard = (() => {
-    // 3x3 board
-    let board = new Array(9);
-    // function to get spot on the board
-    const getSpot = index => board[index];
-    // function to change spot on the board
-    const setSpot = (index, value) => { board[index] = value; };
-    // return the getters and setters
-    return {getSpot, setSpot};
+let gb = document.createElement("div");
+// function to set up the board
+const setBoard = () => {
+  // set class properly
+  gb.setAttribute("id", "board");
+  // add board to body
+  document.body.appendChild(gb);
+};
 
+/* OBJECTS */
+
+// board object
+const board = (() => {
+  setBoard();
+  // 3x3 board
+  let gameBoard = [];
+  let total = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      let sq = document.createElement("div");
+      // add class
+      sq.setAttribute("class", "square");
+      // attribute to get index in the array
+      //sq.setAttribute("data-value", "" + i + j);
+      sq.setAttribute("data-index", "" + total);
+      // append square to grid
+      gb.appendChild(sq);
+
+      gameBoard.push("");
+
+      total++;
+    }
+  }
+  // function to get spot on the board
+  const getSpot = (index) => gameBoard[index];
+  // function to change spot on the board
+  const setSpot = (index, value) => {
+    gameBoard[index] = value;
+  };
+  // return the getters and setters
+  return { getSpot, setSpot };
 })();
 
-const player = (name, marker)  => {
-    // getter for name
-    const getName = () => name;
-    // getter for marker (either 'x' or 'o')
-    const getMarker = () => marker;
+// player object
+const player = (name, marker) => {
+  // getter for name
+  const getName = () => name;
+  // getter for marker (either 'x' or 'o')
+  const getMarker = () => marker;
 
-    // return the getters
-    return {getName, getMarker};
-}
+  // return the getters
+  return { getName, getMarker };
+};
 
-const setBoard = () => {
-    // somehow get gameBoard involved
-    let gb = document.createElement('div');
-    // set class properly
-    gb.setAttribute('id', 'board');
-    // add board to body
-    document.body.appendChild(gb);
+/* FUNCTIONS FOR GAME LOGIC */
 
-    // for loop to create divs for each spot in gameBoard.board
-    for(let i=0; i<9; i++) {
-        // create individual square in grid
-        let sq = document.createElement("div");
-        // add class
-        sq.setAttribute('class', 'square');
-        // attribute to get index in the array
-        sq.setAttribute('data-index', '' + i);
-        // add click event listener to make marker
-        sq.addEventListener('click', (e) => squareClick(e));
-        // append square to grid
-        gb.appendChild(sq);
+// function to display winner/tie information
+const gameOverInfo = () => {
+  const info = document.getElementById("info");
+  if (over) {
+    info.innerHTML = `${currentPlayer.getName()} wins!`;
+  } else {
+    info.innerHTML = "Tie Game, Play Again?";
+  }
+};
+
+// function to check tie: all items in array are filled
+const checkTie = () => {
+  for (let i = 0; i < 9; i++) {
+    if (board.getSpot(i) === "") {
+      return;
     }
-}
+  }
+  tie = true;
+};
 
-
+// function to check whether there is a winner
 const checkWinner = () => {
-    // check rows
-    let over = false;
-    // winning arrays
-    let win = ['x', 'x', 'x'];
-    let win2 = ['o', 'o', 'o'];
+  /* layout of board
+        0 1 2
+        3 4 5
+        6 7 8
+  */
 
+  // get all spots only once
+  let zero = board.getSpot(0);
+  let one = board.getSpot(1);
+  let two = board.getSpot(2);
+  let three = board.getSpot(3);
+  let four = board.getSpot(4);
+  let five = board.getSpot(5);
+  let six = board.getSpot(6);
+  let seven = board.getSpot(7);
+  let eight = board.getSpot(8);
 
-    let rows = [];
-    let cols = [];
-    let diags = [];
-    // row arrays
-    let toprow = [gameBoard.getSpot(0), gameBoard.getSpot(1), gameBoard.getSpot(2)];
-    let midrow = [gameBoard.getSpot(3), gameBoard.getSpot(4), gameBoard.getSpot(5)];
-    let bottomrow = [gameBoard.getSpot(6), gameBoard.getSpot(7), gameBoard.getSpot(8)];
-    // column arrays
-    let onecol = [gameBoard.getSpot(0), gameBoard.getSpot(3), gameBoard.getSpot(6)];
-    let twocol = [gameBoard.getSpot(1), gameBoard.getSpot(4), gameBoard.getSpot(7)];
-    let threecol = [gameBoard.getSpot(2), gameBoard.getSpot(5), gameBoard.getSpot(8)];
-    // diagonal arrays
-    let onediag = [gameBoard.getSpot(0), gameBoard.getSpot(4), gameBoard.getSpot(8)];
-    let twodiag = [gameBoard.getSpot(2), gameBoard.getSpot(4), gameBoard.getSpot(6)];
-    // check rows;
-    
+  // check rows (0 1 2; 3 4 5; 6 7 8)
+  if (
+    (zero === one && zero === two && zero !== "") ||
+    (three === four && three === five && three !== "") ||
+    (six === seven && six === eight && six !== "")
+  ) {
+    over = true;
+  }
+  // check columns(0 3 6; 1 4 7; 2 5 8)
+  if (
+    (zero == three && zero == six && zero !== "") ||
+    (one === four && one === seven && one !== "") ||
+    (two === five && two === eight && two !== "")
+  ) {
+    over = true;
+  }
+  // check diagonals (0 4 8; 2 4 6)
+  if (
+    (zero === four && zero === eight && zero !== "") ||
+    (two === four && two === six && two !== "")
+  ) {
+    over = true;
+  }
 
+  // check tie function
+  checkTie();
 
-    if(((toprow == win || toprow == win2) && toprow != normal)
-    || ((midrow == win || midrow == win2) && midrow != normal)
-    || ((bottomrow == win || bottomrow == win2) && bottomrow != normal)) {
-        over = true;
-    }
-    // check columns
-    else if(((onecol == win || onecol == win2) && onecol != normal)
-    || ((twocol == win || twocol == win2) && twocol != normal)
-    || ((threecol == win || threecol == win2) && threecol != normal)) {
-        over = true;
-    }
-    // check diagonals
-    else if(((onediag == win || onediag == win2) && onediag != normal)
-    || ((twodiag == win || twodiag == win2) && twodiag != normal)) {
-        over = true;
-    }
-    
-    if(over) {
-        const b = document.getElementById('board');
-        for(let i=0; i<b.childNodes.length; i++) {
-            b.children[i].style.border = 'none';
-        }
-        b.innerHTML = `${currentPlayer.getName()} + ' wins!`;
-    }
-    // change the current player to set the proper marker next time
-    if(currentPlayer == ethan) {
-        currentPlayer = bot;
-    }
-    else {
-        currentPlayer = ethan;
-    }
-}
+  if (over || tie) {
+    // hand over control to display game over info
+    gameOverInfo();
+  }
+};
 
-document.addEventListener('click', function(e) {
-    if(e.target.classList.contains('square')) {
-        // local variable for the index of the square in the board
-        let i = parseInt(e.target.dataset.index);
-        // if it is already marked, don't mark it
-        if(gameBoard.getSpot(i) != undefined) {
-            return;
-        }
-        // set the spot in the board to the current marker
-        gameBoard.setSpot(i, currentPlayer.getMarker());
-        // set the inner html to display the current marker
-        e.target.innerHTML = gameBoard.getSpot(i);
+// event listener to track squares getting clicked on
+document.addEventListener("click", function (e) {
+  // if the game is over or if one or more player names have not submitted, do nothing
+  if (over || tie || !playerOne || !playerTwo) {
+    return;
+  }
+  if (e.target.classList.contains("square")) {
+    // local variable for the index of the square in the board
+    let index = e.target.dataset.index;
 
-        // check if the game is over
-        checkWinner();
+    // if it is already marked, just unmark it (redo)
+    if (e.target.innerHTML == "x" || e.target.innerHTML == "o") {
+      e.target.innerHTML = "";
+      return;
     }
+    // set the spot in the board to the current marker
+    board.setSpot(index, currentPlayer.getName());
+    // set the inner html to display the current marker
+    e.target.innerHTML = currentPlayer.getMarker();
+
+    // check if the game is over
+    checkWinner();
+    switchPlayers();
+  }
 });
 
+const switchPlayers = () => {
+  if (currentPlayer == p1) {
+    currentPlayer = p2;
+  } else {
+    currentPlayer = p1;
+  }
+};
 
-let ethan = player('Ethan', 'x');
-let bot = player('Bot', 'o');
-let currentPlayer = ethan;
-setBoard();
+// first player and sign
+let p1;
+// second player and sign
+let p2;
+// is the game over
+let over = false;
+// did the game end in a tie
+let tie = false;
+// has player one readied up
+let playerOne = false;
+// has player two readied up
+let playerTwo = false;
+
+function changeName() {
+  // select player one input
+  const playerInput = document.getElementById("playerOne");
+  // select player one ready button
+  const playerButton = document.getElementById("playerOneButton");
+  // set display to the value of the name inputted
+  const name = playerInput.value;
+  // if name is empty, return
+  if (name === "") {
+    return;
+  }
+  // set display to the value of the name inputted
+  document.getElementById("playerOneDisplay").innerHTML = name;
+  // create the player with the inputted name and the corresponding sign
+  p1 = player(name, "x");
+  // player one is the current player
+  currentPlayer = p1;
+  // get rid of the input and the button
+  playerInput.value = "";
+  playerInput.style.display = "none";
+  playerButton.style.display = "none";
+  // player one has joined the game
+  playerOne = true;
+}
+
+function changeNameTwo() {
+  // select player two input
+  const playerInput = document.getElementById("playerTwo");
+  // select player two ready button
+  const playerButton = document.getElementById("playerTwoButton");
+  // gather the player two input
+  const name = playerInput.value;
+  // if name is empty, return
+  if (name === "") {
+    return;
+  }
+  // set display to the value of the name inputted
+  document.getElementById("playerTwoDisplay").innerHTML = name;
+  // create the player with the inputted name and the corresponding sign
+  p2 = player(name, "o");
+  // get rid of the input and the button
+  playerInput.value = "";
+  playerInput.style.display = "none";
+  playerButton.style.display = "none";
+  // player two has joined the game
+  playerTwo = true;
+}
+
+function restartGame() {
+  let children = document.getElementById("board").childNodes;
+  for (let i = 0; i < children.length; i++) {
+    children[i].innerHTML = "";
+  }
+
+  for (let i = 0; i < 9; i++) {
+    board.setSpot(i, "");
+  }
+
+  currentPlayer = p1;
+  document.getElementById("info").innerHTML = "";
+}
+
+document
+  .getElementById("playerOne")
+  .addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      e.preventDefault();
+    }
+  });
